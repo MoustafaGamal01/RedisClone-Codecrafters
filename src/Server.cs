4eb304e4597d2c950d06174bf4851c7b74215839
Moustafa Gamal <moustafa.gamal672@gmail.com>
2026-03-26 16:57:55 +0200

    codecrafters submit [skip ci]

--- src/Server.cs
+++ src/Server.cs
@@ -1,1 +1,9 @@
-Console.WriteLine("+PONG\\r\\n");
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+var listener = new TcpListener(IPAddress.Any, 6379);
+listener.Start();
+using var client = listener.AcceptTcpClient();
+using var stream = client.GetStream();
+var response = Encoding.ASCII.GetBytes("+PONG\r\n");
+stream.Write(response, 0, response.Length);
