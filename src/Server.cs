4c78eb943d486d1016b534840df00fd5c95be8df
Moustafa Gamal <moustafa.gamal672@gmail.com>
2026-03-26 17:52:32 +0200

    St3, #wy1

--- src/Server.cs
+++ src/Server.cs
@@ -1,9 +1,19 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+
 var listener = new TcpListener(IPAddress.Any, 6379);
 listener.Start();
-using var client = listener.AcceptTcpClient();
-using var stream = client.GetStream();
-var response = Encoding.ASCII.GetBytes("+PONG\r\n");
-stream.Write(response, 0, response.Length);
+
+var client = listener.AcceptTcpClient();
+var stream = client.GetStream();
+var buffer = new byte[1024];
+
+while (true)
+{
+    var bytesRead = stream.Read(buffer, 0, buffer.Length);
+    if (bytesRead == 0) break;
+
+    var response = Encoding.UTF8.GetBytes("+PONG\r\n");
+    stream.Write(response, 0, response.Length);
+}
