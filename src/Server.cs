3666449f743f4168011dbd76fd9625a8c1b3b48f
Moustafa Gamal <moustafa.gamal672@gmail.com>
2026-03-27 15:31:42 +0200

    Handle concurrent clients Stage #zu2

--- src/Server.cs
+++ src/Server.cs
@@ -4,16 +4,24 @@ using System.Text;
 
 var listener = new TcpListener(IPAddress.Any, 6379);
 listener.Start();
-
-var client = listener.AcceptTcpClient();
-var stream = client.GetStream();
-var buffer = new byte[1024];
+Console.WriteLine("Server is listening on port 6379...");
 
 while (true)
 {
-    var bytesRead = stream.Read(buffer, 0, buffer.Length);
-    if (bytesRead == 0) break;
+    var client = listener.AcceptTcpClient();
+    Console.WriteLine("Client connected: " + client.Client.RemoteEndPoint);
+    
+        var stream = client.GetStream();
+        var buffer = new byte[1024];
 
-    var response = Encoding.UTF8.GetBytes("+PONG\r\n");
-    stream.Write(response, 0, response.Length);
+        while (true)
+        {
+            var bytesRead = stream.Read(buffer, 0, buffer.Length);
+            if (bytesRead == 0) break;
+
+            stream.Write(Encoding.UTF8.GetBytes("+PONG\r\n"));
+        }
+    
 }
+// echo "PING`nP`nP`nP" | .\redis-cli.exe -p 6379
+
