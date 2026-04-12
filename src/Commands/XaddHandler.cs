dee7285c9d584f371b9399edf1f1f03f62d664c5
Moustafa Gamal <moustafa.gamal672@gmail.com>
2026-04-12 11:09:37 +0200

    feat:add Create a stream #cf6

---
+++ src/Commands/XaddHandler.cs
@@ -0,0 +1,30 @@
+﻿using codecrafters_redis.src.Core;
+using codecrafters_redis.src.IRepository;
+using codecrafters_redis.src.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codecrafters_redis.src.Commands
+{
+    internal class XaddHandler : ICommandHandler
+    {
+        private readonly Store _store;
+        public XaddHandler(Store store)
+        {
+            _store = store;
+        }
+
+        public CommandsName CommandName => CommandsName.XADD;
+
+        public async Task Handle(NetworkStream stream, List<string> parts)
+        {
+            _store.XADD(parts[1],   parts[2], parts.Skip(3).ToDictionary(k => k, v => v));
+
+            await RespWriter.WriteBulkString(stream, parts[2]);
+        }
+    }
+}
---
+++ src/Core/RedisStream.cs
@@ -0,0 +1,14 @@
+﻿using codecrafters_redis.src.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codecrafters_redis.src.Core
+{
+    internal class RedisStream : RedisValue
+    {
+        public List<Dictionary<string, string>> Entries { get; } = new List<Dictionary<string, string>>();
+    }
+}
