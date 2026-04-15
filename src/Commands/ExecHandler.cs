7b48350b1b54895f0d285b612118d0b1567d6ec0
Moustafa Gamal <moustafa.gamal672@gmail.com>
2026-04-15 17:29:42 +0200

    The EXEC command #lo4

---
+++ src/Commands/ExecHandler.cs
@@ -0,0 +1,30 @@
+﻿using codecrafters_redis.src.Core;
+using codecrafters_redis.src.IRepository;
+using codecrafters_redis.src.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codecrafters_redis.src.Commands
+{
+    internal class ExecHandler : ICommandHandler
+    {
+        private readonly Store _store;
+        public ExecHandler(Store store)
+        {
+            _store = store;
+        }
+
+        public CommandsName CommandName => CommandsName.EXEC;
+
+        public async Task Handle(NetworkStream stream, List<string> parts)
+        {
+            var result = _store.EXEC(); 
+
+            await RespWriter.WriteError(stream, result);
+        }
+    }
+}
