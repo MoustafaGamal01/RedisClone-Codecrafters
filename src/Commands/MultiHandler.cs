40ac0c20dcc036a4c89ca43b81e479958857e260
Moustafa Gamal <moustafa.gamal672@gmail.com>
2026-04-15 16:47:04 +0200

    feat:add The MULTI command #pn0

---
+++ src/Commands/MultiHandler.cs
@@ -0,0 +1,27 @@
+﻿using codecrafters_redis.src.Core;
+using codecrafters_redis.src.IRepository;
+using codecrafters_redis.src.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codecrafters_redis.src.Commands
+{
+    internal class MultiHandler : ICommandHandler
+    {
+        private readonly Store _store;
+        public MultiHandler(Store store) { _store = store; }
+
+        public CommandsName CommandName => CommandsName.MULTI;
+
+        public async Task Handle(NetworkStream stream, List<string> parts)
+        {
+            var result = _store.MULTI(); 
+        
+            await RespWriter.WriteSimpleString(stream, result);
+        }
+    }
+}
