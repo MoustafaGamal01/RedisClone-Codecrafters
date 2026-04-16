e93ea86894522d322663360f2b4d17173635473d
Moustafa Gamal <moustafa.gamal672@gmail.com>
2026-04-16 16:12:44 +0200

    Queueing commands #rs9

---
+++ src/Core/ClientContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codecrafters_redis.src.Core
+{
+    public class ClientContext
+    {
+        public bool IsInTransaction { get; set; } = false;
+        public Queue<List<string>> CommandQueue { get; } = new();
+    }
+}
