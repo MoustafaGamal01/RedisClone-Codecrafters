6708aa39b4032ca1ee591b4399745fe0b0048f56
Moustafa Gamal <moustafa.gamal672@gmail.com>
2026-04-17 21:23:37 +0200

    The WATCH command #jb7

---
+++ src/Commands/WatchHandler.cs
@@ -0,0 +1,16 @@
+﻿
+using codecrafters_redis.src.Core;
+using codecrafters_redis.src.IRepository;
+using codecrafters_redis.src.Protocol;
+using System.Net.Sockets;
+
+namespace codecrafters_redis.src.Commands;
+
+internal class WatchHandler : ICommandHandler
+{
+    public CommandsName CommandName => CommandsName.WATCH;
+    public async Task Handle(NetworkStream stream, List<string> parts, ClientContext context)
+    {
+        await RespWriter.WriteSimpleString(stream, "OK");
+    }
+}
