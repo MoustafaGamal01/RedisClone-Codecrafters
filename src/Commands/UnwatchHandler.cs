d3aa8b2831b95c92d039d78f056c6f0c498a6c58
Moustafa Gamal <moustafa.gamal672@gmail.com>
2026-04-18 17:47:58 +0200

    codecrafters submit [skip ci]

---
+++ src/Commands/UnwatchHandler.cs
@@ -0,0 +1,18 @@
+﻿
+using codecrafters_redis.src.Core;
+using codecrafters_redis.src.IRepository;
+using codecrafters_redis.src.Protocol;
+using System.Net.Sockets;
+
+namespace codecrafters_redis.src.Commands;
+
+internal class UnwatchHandler : ICommandHandler
+{
+    public CommandsName CommandName => CommandsName.UNWATCH;
+    public async Task Handle(NetworkStream stream, List<string> parts, ClientContext context)
+    {
+        context.WatchedKeys.Clear();
+        await RespWriter.WriteSimpleString(stream, "OK");
+    }
+}
+
