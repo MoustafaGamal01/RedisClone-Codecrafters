499673d348e79dccd9b6ffedb1dc7a300a770e18
Moustafa Gamal <moustafa.gamal672@gmail.com>
2026-04-18 18:28:55 +0200

    feat: add Configure listening port #bw1

--- src/Program.cs
+++ src/Program.cs
@@ -3,14 +3,32 @@ using codecrafters_redis.src.Core;
 using System.Net;
 using System.Net.Sockets;
 
-var store = new Store();
-var dispatcher = new CommandHandler(store);
-var clientHandler = new ClientHandler(dispatcher);
-var listener = new TcpListener(IPAddress.Any, 6379);
-listener.Start();
 
-while (true)
+class Program
 {
-    var client = await listener.AcceptTcpClientAsync();
-    _ = Task.Run(() => clientHandler.HandleAsync(client));
+    static async Task Main(string[] args)
+    {
+        int port = 6379; // default
+
+        for (int i = 0; i < args.Length; i++)
+        {
+            if (args[i] == "--port" && i + 1 < args.Length)
+            {
+                port = int.Parse(args[i + 1]);
+            }
+        }
+
+        var store = new Store();
+        var dispatcher = new CommandHandler(store);
+        var clientHandler = new ClientHandler(dispatcher);
+        var listener = new TcpListener(IPAddress.Any, port);
+        listener.Start();
+
+        while (true)
+        {
+            var client = await listener.AcceptTcpClientAsync();
+            _ = Task.Run(() => clientHandler.HandleAsync(client));
+        }
+
+    }
 }
