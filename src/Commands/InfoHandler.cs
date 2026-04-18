21f17dda3a5356153f612b98d383a6c4d89b5a5d
Moustafa Gamal <moustafa.gamal672@gmail.com>
2026-04-18 19:40:54 +0200

    feat:add The INFO command #ye5

---
+++ src/Commands/InfoHandler.cs
@@ -0,0 +1,32 @@
+﻿
+using codecrafters_redis.src.Core;
+using codecrafters_redis.src.IRepository;
+using codecrafters_redis.src.Protocol;
+using System.Net.Sockets;
+
+namespace codecrafters_redis.src.Commands;
+
+internal class InfoHandler : ICommandHandler
+{
+    public CommandsName CommandName => CommandsName.INFO;
+
+    private string GetReplicationInfo()
+    {
+        return "# Replication\r\nrole:master\r\nconnected_slaves:0";
+    }
+
+    public async Task Handle(NetworkStream stream, List<string> parts, ClientContext context)
+    {
+        var key = parts.Count > 1 ? parts[1] : "default";
+
+        switch (key.ToUpper())
+        {
+            case "REPLICATION":
+                await RespWriter.WriteBulkString(stream, GetReplicationInfo());
+                break;
+            default:
+                await RespWriter.WriteError(stream, $"Unsupported INFO section: {key}");
+                break;
+        }
+    }
+}
