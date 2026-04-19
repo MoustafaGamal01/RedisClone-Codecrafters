7e61cfa4a53fe6f773d7505c9284f557f3fd1b8b
Moustafa Gamal <moustafa.gamal672@gmail.com>
2026-04-19 20:12:35 +0200

    codecrafters submit [skip ci]

---
+++ src/Commands/PsyncHandler.cs
@@ -0,0 +1,28 @@
+﻿global using codecrafters_redis.src.Core;
+global using codecrafters_redis.src.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codecrafters_redis.src.Commands;
+
+internal class PsyncHandler  : ICommandHandler
+{
+    public CommandsName CommandName => CommandsName.PSYNC;
+    public async Task Handle(NetworkStream stream, List<string> parts, ClientContext context) 
+    {
+        if (parts.Count != 3)
+        {
+            await stream.WriteAsync(Encoding.UTF8.GetBytes("wrong number of arguments for 'psync' command\r\n"));
+            return;
+        }
+        string runId = "8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb";
+        long offset = 0;
+
+        await stream.WriteAsync(Encoding.UTF8.GetBytes($"+FULLRESYNC {runId} {offset}\r\n"));
+    }
+    
+}
