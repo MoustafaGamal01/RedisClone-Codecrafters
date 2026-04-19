a45e22ee80eeaa938c7e1b487b00d258c3534e95
Moustafa Gamal <moustafa.gamal672@gmail.com>
2026-04-19 19:38:59 +0200

    feat: add Receive handshake (1/2) #fj0

---
+++ src/Commands/ReplConfHandler.cs
@@ -0,0 +1,22 @@
+﻿using codecrafters_redis.src.Core;
+using codecrafters_redis.src.IRepository;
+using codecrafters_redis.src.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codecrafters_redis.src.Commands;
+
+internal class ReplConfHandler : ICommandHandler
+{
+    public CommandsName CommandName => CommandsName.REPLCONF;
+
+    public async Task Handle(NetworkStream stream, List<string> parts, ClientContext context)
+    {
+        await RespWriter.WriteSimpleString(stream, "OK");
+    }
+}
+
