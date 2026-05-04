9a55445bd38be5dc4efe95d64e38703e6d52acd6
Moustafa Gamal <moustafa.gamal672@gmail.com>
2026-05-04 15:27:09 +0300

    WAIT with no replicas #my8

---
+++ src/Commands/WaitHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codecrafters_redis.src.Commands;
+
+internal class WaitHandler : ICommandHandler
+{
+    public CommandsName CommandName => CommandsName.WAIT;
+
+    public async Task Handle(NetworkStream stream, List<string> parts, ClientContext context)
+    {
+        if (parts.Count < 3)
+        {
+            await RespWriter.WriteError(stream, "WAIT requires at least 3 arguments");
+            return;
+        }
+
+        await RespWriter.WriteInteger(stream, 0);
+    }
+}
