d439e461e18c0415736e861b5a545765ce3fdda9
Moustafa Gamal <moustafa.gamal672@gmail.com>
2026-05-04 19:27:39 +0300

    Read a key #jz6

---
+++ src/Commands/KeysHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codecrafters_redis.src.Commands;
+
+internal class KeysHandler : ICommandHandler
+{
+    private readonly Store _store;
+    public KeysHandler(Store store)
+    {
+        _store = store;
+    }
+    public CommandsName CommandName => CommandsName.KEYS;
+    public async Task Handle(NetworkStream stream, List<string> parts, ClientContext context)
+    {
+        if (parts.Count != 2)
+        {
+            await RespWriter.WriteError(stream, "ERR wrong number of arguments for 'KEYS' command");
+            return;
+        }
+
+        var keys = _store.KEYS(parts[1]);
+        await RespWriter.WriteArray(stream, keys);
+    }
+}
