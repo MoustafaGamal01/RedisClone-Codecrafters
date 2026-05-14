adf1c9831c76a5438d1a8eb9a5e090a04148790c
Moustafa Gamal <moustafa.gamal672@gmail.com>
2026-05-14 11:57:14 +0300

    feat:fix pushing Subscribe to a channel #mx3

---
+++ src/Commands/SubscribeHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codecrafters_redis.src.Commands;
+
+internal class SubscribeHandler : ICommandHandler
+{
+    public CommandsName CommandName => CommandsName.SUBSCRIBE;
+    private readonly Store _store;
+    public SubscribeHandler(Store store)
+    {
+        _store = store;
+    }
+
+    public async Task Handle(NetworkStream stream, List<string> parts, ClientContext context)
+    {
+        var channel = parts[1];
+
+        context.SubscribedChannels.Add(channel);
+        var count = context.SubscribedChannels.Count;
+
+        var response = $"*3\r\n$9\r\nsubscribe\r\n${channel.Length}\r\n{channel}\r\n:{count}\r\n";
+        await stream.WriteAsync(Encoding.UTF8.GetBytes(response));
+    }
+}
