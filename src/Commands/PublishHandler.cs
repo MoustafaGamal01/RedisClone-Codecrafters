67ec57b7b198d81ac5fcb9b772eeb75fa09b655e
Moustafa Gamal <moustafa.gamal672@gmail.com>
2026-05-14 12:53:53 +0300

    feat:add Publish a message

---
+++ src/Commands/PublishHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codecrafters_redis.src.Commands;
+
+internal class PublishHandler : ICommandHandler {
+    public CommandsName CommandName => CommandsName.PUBLISH;
+
+    public readonly Store _store;
+    public PublishHandler(Store store)
+    {
+        _store = store;
+    }
+
+    public async Task Handle(NetworkStream stream, List<string> parts, ClientContext context)
+    {
+        if (parts.Count < 3)
+        {
+            await RespWriter.WriteError(stream, "Wrong number of arguments for 'PUBLISH' command");
+            return;
+        }
+
+        var channel = parts[1];
+        var count = _store.GetSubscriberCount(channel);
+        await RespWriter.WriteInteger(stream, count);
+    }
+}
