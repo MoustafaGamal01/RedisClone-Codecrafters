1e04a45acb6cd7ad0f1e6c28537fa6f077eb5e3d
Moustafa Gamal <moustafa.gamal672@gmail.com>
2026-05-15 13:53:45 +0300

    feat:add Retrieve member rank #lg6

---
+++ src/Commands/ZrankHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codecrafters_redis.src.Commands;
+
+internal class ZrankHandler : ICommandHandler
+{
+    private readonly Store _store;
+    public ZrankHandler(Store store)
+    {
+        _store = store;
+    }
+    public CommandsName CommandName => CommandsName.ZRANK;
+
+    public async Task Handle(NetworkStream stream, List<string> parts, ClientContext context)
+    {
+        if(parts.Count != 3)
+        {
+            await RespWriter.WriteError(stream, "wrong number of arguments for 'zrank' command\r\n");
+            return;
+        }
+
+        var result = _store.ZRANK(parts[1], parts[2]);
+
+        if (result == -1) 
+            await RespWriter.WriteNullBulkString(stream);
+        else
+            await RespWriter.WriteInteger(stream, result);
+    }
+}
