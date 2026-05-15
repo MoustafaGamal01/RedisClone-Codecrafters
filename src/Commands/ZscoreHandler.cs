2a0bd8aea06c0defbe7656e0f80cb8b049d23e64
Moustafa Gamal <moustafa.gamal672@gmail.com>
2026-05-15 17:49:22 +0300

    feat:add Retrieve member score #gd7

---
+++ src/Commands/ZscoreHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codecrafters_redis.src.Commands;
+
+internal class ZscoreHandler : ICommandHandler
+{
+    private readonly Store _store;
+    public ZscoreHandler(Store store)
+    {
+        _store = store;
+    }
+    public CommandsName CommandName => CommandsName.ZSCORE;
+    public async Task Handle(NetworkStream stream, List<string> parts, ClientContext context)
+    {
+        if(parts.Count < 3)
+        {
+            await RespWriter.WriteError(stream, "wrong number of arguments from ZSCORE command");
+            return;
+        }
+
+        var result = _store.ZSCORE(parts[1], parts[2]);
+
+        if (result == -1)
+            await RespWriter.WriteNullBulkString(stream);
+        else
+            await RespWriter.WriteBulkString(stream, result.ToString());
+    }
+}
