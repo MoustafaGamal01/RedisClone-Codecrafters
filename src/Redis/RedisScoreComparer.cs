c761e1d05a6459b7505c1e9ed8612ae8bb39792e
Moustafa Gamal <moustafa.gamal672@gmail.com>
2026-05-16 18:57:33 +0300

    feat:add Arch RedisSortedSetComperer

---
+++ src/Redis/RedisScoreComparer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codecrafters_redis.src.Redis;
+
+internal class RedisScoreComparer : IComparer<(double score, string value)>
+{
+    public static readonly RedisScoreComparer Instance = new();
+    public int Compare((double score, string value) x, (double score, string value) y)
+    {
+        int cmp = x.score.CompareTo(y.score);
+        return cmp != 0 ? cmp : string.Compare(x.value, y.value, StringComparison.Ordinal);
+    }
+}
