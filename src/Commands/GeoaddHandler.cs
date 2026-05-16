abd5f209afe6d4b7e8f8846e6cad56294ec06059
Moustafa Gamal <moustafa.gamal672@gmail.com>
2026-05-16 18:40:03 +0300

    feat: add Respond to GEOADD #zt4

---
+++ src/Commands/GeoaddHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codecrafters_redis.src.Commands;
+
+internal class GeoaddHandler : ICommandHandler
+{
+    private readonly Store _store;
+    public GeoaddHandler(Store store)
+    {
+        _store = store;
+    }
+
+    public CommandsName CommandName => CommandsName.GEOADD;
+
+    public async Task Handle(NetworkStream stream, List<string> parts, ClientContext context)
+    {
+        if(parts.Count < 5 || parts.Count % 2 == 0)
+        {
+            await RespWriter.WriteError(stream,"wrong number of arguments for GEOADD command");
+            return;
+        }
+
+
+        var key = parts[1];
+        var logitude = double.Parse(parts[2]);
+        var latitude = double.Parse(parts[3]);
+        var place = parts[4];
+
+        var added = _store.GEOADD(key, logitude, latitude, place);
+    
+        await RespWriter.WriteInteger(stream, added);
+    }
+}
