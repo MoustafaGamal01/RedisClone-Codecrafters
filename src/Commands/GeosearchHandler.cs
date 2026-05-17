5a00db3024f75344f46041796867ee0430c7479e
Moustafa Gamal <moustafa.gamal672@gmail.com>
2026-05-17 17:00:45 +0300

    adding GeoSearchHandler file

---
+++ src/Commands/GeosearchHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codecrafters_redis.src.Commands;
+
+internal class GeosearchHandler : ICommandHandler
+{
+    private readonly Store _store;
+    public GeosearchHandler(Store store)
+    {
+        _store = store;
+    }
+    public CommandsName CommandName => CommandsName.GEOSEARCH;
+
+    public async Task Handle(NetworkStream stream, List<string> parts, ClientContext context)
+    {
+        // > GEOSEARCH places FROMLONLAT 2 48 BYRADIUS 100 m
+
+        var key = parts[1];
+        var lon = double.Parse(parts[3]);
+        var lat = double.Parse(parts[4]);
+        var dis = double.Parse(parts[6]);
+
+        var result = _store.GEOSEARCH(key, lon, lat, dis);
+
+        await RespWriter.WriteArray(stream, result);
+
+    }
+}
