6b7a3e4ab8bab44ddf3085655ed1b55fa8f00963
Moustafa Gamal <moustafa.gamal672@gmail.com>
2026-05-18 13:14:10 +0300

    adding WhoAmIHandler file

---
+++ src/Commands/WhoAmIHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codecrafters_redis.src.Commands;
+
+internal class WhoAmIHandler : ICommandHandler
+{
+    public CommandsName CommandName => CommandsName.WHOAMI;
+
+    public async Task Handle(NetworkStream stream, List<string> parts, ClientContext context)
+    {
+        if(parts.Count != 2)
+        {
+            await RespWriter.WriteError(stream, "wrong number of arguments for 'whoami' command");
+            return;
+        }
+
+        await RespWriter.WriteBulkString(stream, "default");
+    }
+}
